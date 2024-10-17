6474bee80f5b473f65c314d0b7f5588f8b0434a1
AlimatouSadiyah <tallalimatou@ept.sn>
2024-10-17 15:30:23 +0000

    Add files via upload

--- script.sh
+++ script.sh
@@ -8,12 +8,6 @@ log() {
 command -v curl >/dev/null 2>&1 || { log "curl is required but not installed. Aborting."; exit 1; }
 command -v pip >/dev/null 2>&1 || { log "pip is required but not installed. Aborting."; exit 1; }
 
-# Installer Open-WebUI dans un environnement virtuel
-log "Setting up virtual environment and installing Open-WebUI..."
-python3 -m venv venv
-source venv/bin/activate
-pip install open-webui --ignore-installed blinker || { log "Failed to install Open-WebUI"; exit 1; }
-
 # Installation d'Ollama
 log "Downloading and installing Ollama..."
 (curl -fsSL https://ollama.com/install.sh | sh) || { log "Failed to install Ollama"; exit 1; }
@@ -32,15 +26,18 @@ for i in {1..10}; do
     sleep 1
 done
 
-# Si Ollama ne se lance pas après 10 tentatives
-if ! nc -z 127.0.0.1 11434; then
-    log "Failed to start Ollama on port 11434. Aborting."
-    exit 1
-fi
 
 # Téléchargement du modèle
 log "Downloading model..."
 ollama run hf.co/bartowski/Llama-3.1-WhiteRabbitNeo-2-70B-GGUF:latest || { log "Model download failed"; exit 1; }
+
+
+# Installer Open-WebUI dans un environnement virtuel
+log "Setting up virtual environment and installing Open-WebUI..."
+python3 -m venv venv
+source venv/bin/activate
+pip install open-webui --ignore-installed blinker || { log "Failed to install Open-WebUI"; exit 1; }
+
 
 # Création du modèle
 log "Creating model..."
