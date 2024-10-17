cb2fd20a2ec689bd302ecb91901bc6ade1729656
AlimatouSadiyah <tallalimatou@ept.sn>
2024-10-17 14:55:55 +0000

    Add files via upload

--- script.sh
+++ script.sh
@@ -5,9 +5,14 @@ log() {
 }
 
 # Vérification des dépendances
-command -v git >/dev/null 2>&1 || { log "git is required but not installed. Aborting."; exit 1; }
 command -v curl >/dev/null 2>&1 || { log "curl is required but not installed. Aborting."; exit 1; }
 command -v pip >/dev/null 2>&1 || { log "pip is required but not installed. Aborting."; exit 1; }
+
+# Installer Open-WebUI dans un environnement virtuel
+log "Setting up virtual environment and installing Open-WebUI..."
+python3 -m venv venv
+source venv/bin/activate
+pip install open-webui --ignore-installed blinker || { log "Failed to install Open-WebUI"; exit 1; }
 
 # Installation d'Ollama
 log "Downloading and installing Ollama..."
@@ -19,7 +24,7 @@ ollama &
 
 # Vérification du démarrage d'Ollama
 log "Waiting for Ollama to start..."
-for i in {1..10}; do
+for i in {1..3}; do
     if pgrep -x "ollama" > /dev/null; then
         log "Ollama started successfully."
         break
@@ -31,21 +36,8 @@ done
 log "Downloading model..."
 ollama run hf.co/bartowski/Llama-3.1-WhiteRabbitNeo-2-70B-GGUF:latest || { log "Model download failed"; exit 1; }
 
-# Clonage du dépôt Cyberllm
-log "Cloning repository..."
-git clone https://github.com/BachirTra/Cyberllm || { log "Failed to clone repository Cyberllm"; exit 1; }
-cd Cyberllm || { log "Failed to change directory to Cyberllm"; exit 1; }
 
 # Création du modèle
 log "Creating model..."
 ollama create test1 -f Modelfile || { log "Failed to create model"; exit 1; }
 
-# Installer Open-WebUI dans un environnement virtuel
-log "Setting up virtual environment and installing Open-WebUI..."
-python3 -m venv venv
-source venv/bin/activate
-pip install open-webui --ignore-installed blinker || { log "Failed to install Open-WebUI"; exit 1; }
-
-# Vérifier les permissions du script
-chmod +x script.sh
-log "Setup completed successfully."
